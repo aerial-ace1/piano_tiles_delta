ffceb212c03a6fa2e0746035854f296017c79d8d
aerial-ace1 <96421655+aerial-ace1@users.noreply.github.com>
2022-06-01 16:25:41 +0530

    no improv

--- scriptnormal.js
+++ scriptnormal.js
@@ -1,11 +1,13 @@
 const tiles = Array.from(document.querySelectorAll(".tile"));
 const playbut = document.getElementById('play');
+socringBoard = document.getElementsByClassName('value');
 var counter;
 var score;
 var selected;
 var tileStrokes;
 var userStrokes;
 var outcome;
+var roundOver = false;
 var key = {1:'A1',2:'A2',3:'A3',4:'A4',5:'B1',6:'B2',7:'B3',8:'B4',9:'C1',10:'C2',11:'C3',12:'C4',13:'D1',14:'D2',15:'D3',16:'D4'}
 
 
@@ -30,9 +32,9 @@ function generateTile (iterator, tileArray){
     
     for (let i=0; i<iterator;i++){
         code = (Math.floor(Math.random() * Object.keys(tileArray).length) +1);
-        toplay = document.querySelector(`[data-key="${key[code]}"`)
+        let toplay = document.querySelector(`[data-key="${key[code]}"`);
         tplay(toplay);
-        //Delay to add
+        
         tileArray[key[code]]+=1;
     }
     return 0;
@@ -56,20 +58,23 @@ function checkTiles ( code , input){
 }
 
 function userInput(e){
+
     play(e);
     clickedTile = e.target.getAttribute('data-key');
     userStrokes[clickedTile] += 1;
     selected += 1;
+
     if (selected === counter){
         tiles.forEach(tile => tile.removeEventListener("click", userInput));
         outcome = checkTiles(tileStrokes,userStrokes);
         console.log(outcome);
+        roundOver = true;
         return 0;
     }
 }
 
 function round(roundNo){
-    playbut.setAttribute('disabled','');
+    playbut.disabled = true;
     playbut.innerHTML='Playing.....';
     tiles.forEach(tile => tile.removeEventListener('click',play));
 
@@ -78,8 +83,19 @@ function round(roundNo){
     userStrokes = generateTrack();
     
     generateTile(roundNo , tileStrokes)
+    console.log(tileStrokes);
     tiles.forEach(tile => tile.addEventListener("click", userInput));
     
+    if (roundOver === true && selected === roundNo){
+        return outcome;
+    }
+    
+}
+
+function reset(){
+    socringBoard.innerHTML = '';
+    playbut.disabled = false;
+    playbut.innerHTML='Play';
 }
 
 playbut.addEventListener('click',startgame);
@@ -88,9 +104,11 @@ tiles.forEach(tile => tile.addEventListener('click',play));
 function startgame(e){
     counter = 2;
     score = 0;
-
+    socringBoard.innerHTML = score;
     //loop over
     counter += 1;
+    roundOver = false;
     round(counter);
+    
 }
 
